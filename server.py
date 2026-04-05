61bfbecc8d2c86496c0b76c258e32b3e1f37b79c
stevenands-glitch <stevenandz92491@gmail.com>
2026-04-05 00:09:30 -0400

    but I like margot robbie more

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from EmotionDetection.emotion_detection import emotion_detector
-from flask import Flask, render_template, request
-
 """
 This module starts the main server
 to run the application
 """
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
 application = Flask("Emotion Detection")
 
 @application.route("/")
@@ -25,17 +26,19 @@ def emotion_detection():
     text = request.args.get("textToAnalyze")
     response = emotion_detector(text)
 
-    if type(response) == dict and response["dominant_emotion"] is not None:
-        response_text = "For the given statement, the system response is {}. The dominant emotion is {}."
+    if isinstance(response, dict) and response["dominant_emotion"] is not None:
+        response_text = """
+            For the given statement,
+            the system response is {}. The dominant emotion is {}.
+        """
         all_pairs = ""
         for em, sc in response.items():
-            if em is not "dominant_emotion":
+            if em != "dominant_emotion":
                 all_pairs += f"\'{em}\': {sc}, "
 
         all_pairs = all_pairs[0:-2]
         return response_text.format(all_pairs, response['dominant_emotion'])
-    else:
-        return "Invalid text! Please try again!"
+    return "Invalid text! Please try again!"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=5001)
